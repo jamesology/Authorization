e6a4a7c5e69327c58bd9addd2f87af6eb2200f0d
James Gerstmann <jamesology@gmail.com>
2014-02-12 20:51:13 -0600

    create command line

---
+++ Authorization.Cli/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Authorization.Cli
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//TODO: fire up structuremap
+
+			while (args.Any() == false)
+			{
+				Console.WriteLine("Type something will you, we're paying for this.");
+
+				var input = Console.ReadLine();
+
+				if (String.IsNullOrWhiteSpace(input) == false)
+				{
+					args = input.Split(' ');
+				}
+			}
+
+			//TODO: process command
+			foreach (var word in args)
+			{
+				Console.Write("{0} ", word);
+			}
+			Console.ReadLine();
+		}
+	}
+}
