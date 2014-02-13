b0ecee061f3b2284c5baf9e37ec9ac0fff326ef8
James Gerstmann <jamesology@gmail.com>
2014-02-12 20:35:28 -0600

    Create solution. Initial objects.

---
+++ Authorization/Role.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Authorization
+{
+	public class Role
+	{
+		public string Name { get; set; }
+
+		public ICollection<string> Actions { get; set; }
+
+		public Role()
+		{
+			Actions = new Collection<string>();
+		}
+	}
+}
---
+++ Authorization/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Authorization
+{
+	public class User
+	{
+		public string Name { get; set; }
+
+		public ICollection<string> Roles { get; set; }
+
+		public User()
+		{
+			Roles = new Collection<string>();
+		}
+	}
+}
