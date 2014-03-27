11eb5b1833592870f9b17eeb725c49ef68ee4794
James G <james.gerstmann@gmail.com>
2014-03-26 21:53:27 -0500

    starting anew

---
+++ Authorization.Web/Controllers/CanController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Authorization.Web.Controllers
+{
+	[RoutePrefix("can")]
+	public class CanController : ApiController
+	{
+		[Route("{uuser}/{aaction}")]
+		[HttpGet]
+		public HttpResponseMessage Get(string uuser, string aaction)
+		{
+			var responseMessage = String.Format("User {0} can {1}.", uuser, aaction);
+
+			var result = new HttpResponseMessage
+			{
+				Content = new StringContent(responseMessage)
+			};
+
+			return result;
+		}
+	}
+}
