26cc48d3e19c08e9a81bbe18d5caea8018e3e8e5
jamesology <james.gerstmann@gmail.com>
2014-09-17 13:07:56 -0500

    wire-up cli utility

---
+++ Authorization.Cli/Commands/DefaultCommands.cs
@@ -0,0 +1,18 @@
+﻿namespace Authorization.Cli.Commands
+{
+	public static class DefaultCommands
+	{
+		public static string Status()
+		{
+			return "The app is running.";
+		}
+	}
+
+	public static class Core
+	{
+		public static string AddUser()
+		{
+			return "This will add a user. But it doesn't yet.";
+		}
+	}
+}
